7831973a797e3b9730dd75dd9c844359d7c79808
Holly Klose <hollyklose@gmail.com>
2016-03-07 13:31:26 -0600

    first ggit commit

---
+++ Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Social-Silliness
+//
+//  Created by Holly Klose on 3/7/16.
+//  Copyright © 2016 Holly Klose. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR: CGFloat = 157.0 / 255.0
+
