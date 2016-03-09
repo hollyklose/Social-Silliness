27bf5627a59ab3b9f2565ce348cd03cd4cd7a07e
Holly Klose <hollyklose@gmail.com>
2016-03-09 14:29:31 -0700

    after building table UI

---
+++ FeedVC.swift
@@ -0,0 +1,35 @@
+//
+//  FeedVC.swift
+//  Social-Silliness
+//
+//  Created by Holly Klose on 3/9/16.
+//  Copyright © 2016 Holly Klose. All rights reserved.
+//
+
+import UIKit
+
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCellWithIdentifier("PostCell") as! PostCell
+    }
+
+
+}
